a9235ec4f0b45e833ab1861e1eef3ba4964859df
Tim Zaleski <zaleskitimothy@yahoo.com>
2021-02-28 01:06:37 -0700

    auth

--- keepr/Startup.cs
+++ keepr/Startup.cs
@@ -19,17 +19,16 @@ using keepr.Services;
 
 namespace keepr
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+      Configuration = configuration;
+    }
 
-        public IConfiguration Configuration { get; }
+    public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+    public void ConfigureServices(IServiceCollection services)
     {
       // REVIEW[epic=Authentication] creates functionality for authentication
       services.AddAuthentication(options =>
@@ -56,53 +55,59 @@ namespace keepr
               });
         });
 
-            // TODO Transient Repositories
-            services.AddTransient<VaultsRepository>();
-            services.AddTransient<ProfilesRepository>();
-            services.AddTransient<KeepsRepository>();
-            services.AddTransient<VaultKeepsRepository>();
+        // TODO Transient Repositories
+        services.AddTransient<VaultsRepository>();
+        services.AddTransient<ProfilesRepository>();
+        services.AddTransient<KeepsRepository>();
+        services.AddTransient<VaultKeepsRepository>();
 
-            // TODO Transient Services
-            services.AddTransient<VaultsService>();
-            services.AddTransient<ProfilesService>();
-            services.AddTransient<KeepsService>();
-            services.AddTransient<VaultKeepsService>();
+        // TODO Transient Services
+        services.AddTransient<VaultsService>();
+        services.AddTransient<ProfilesService>();
+        services.AddTransient<KeepsService>();
+        services.AddTransient<VaultKeepsService>();
 
-            services.AddControllers();
 
-            services.AddScoped<IDbConnection>(x => CreateDbConnection());
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "keepr", Version = "v1" });
-            });
-        }
+      services.AddControllers();
 
-        private IDbConnection CreateDbConnection()
-        {
-            var connectionString = Configuration["db:gearhost"];
-            return new MySqlConnection(connectionString);
-        }
+      services.AddScoped<IDbConnection>(x => CreateDbConnection());
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "keepr v1"));
-            }
 
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "keepr", Version = "v1" });
+      });
     }
+
+    private IDbConnection CreateDbConnection()
+    {
+      var connectionString = Configuration["db:gearhost"];
+      return new MySqlConnection(connectionString);
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "keepr v1"));
+        app.UseCors("CorsDevPolicy");
+      }
+
+      app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.UseAuthentication();
+
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
 }
